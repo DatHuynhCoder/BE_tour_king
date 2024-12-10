d5e87c45d3a427db4591f5673727038014a95508
DatHuynhCoder <htdat2004@gmail.com>
2024-12-10 22:35:31 +0700

    Setting db

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express()
+
+//Kết nối tới database crud trên MySQL mở bằng Xampp
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'tour_king'
+})
+
+app.get("/", (req,res) => {
+  res.json("Hello backend")
+})
 
 app.listen(8800, () => {
   console.log("Connected to Backend. Keep moving forward");
